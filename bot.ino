47e7e004f0842a7d6ecead55251a639b5efa1c98
shreyaskulur <shreyaskulur@gmail.com>
2015-10-11 02:42:55 +0530

    2
    2

--- bot.ino
+++ bot.ino
@@ -17,6 +17,8 @@ int on[6];  //present on readings
 int off[6];  //present off readings
 int diff[6]; //present diff
 
+int lcount = 0;
+int rcount = 0;
 void left ( int turn_speed = 255)
 {
   analogWrite(left_speed , turn_speed);
@@ -87,9 +89,52 @@ stp();
 delay(1000000);
 }
 
+void find_diff()
+{
+  digitalWrite(13, LOW);
+  Serial.print("off  ");
+  delay(50);
+  for( int i =0; i<5; i++)
+  {
+    off[i] = analogRead(i);
+     Serial.print(off[i] );
+      Serial.print("\t");
+  }
+  Serial.print("\n");
+  delay(50);
+  
+  digitalWrite(13,HIGH);
+  Serial.print("on  ");
+  delay(50);
+  for( int i =0; i<5; i++)
+  {
+    on[i] = analogRead(i);
+     Serial.print( on[i]);
+      Serial.print("\t");
+  }
+  Serial.print("\n");
+  delay(50);
+  
+  Serial.print("Diff  ");
+  for( int i =0; i<5; i++)
+  {
+    diff[i] = on[i] - off[i];;
+     Serial.print( diff[i]);
+      Serial.print("\t");
+  }
+  Serial.print("\n");  
+}
+
 void bot_setup()
 {
-  left();
+  find_diff();
+  high[0] = analogRead(diff[0]);
+  high[1] = analogRead(diff[1]);
+  high[3] = analogRead(diff[3]);
+  high[4] = analogRead(diff[4]);
+  high[2] = (high[0] + high[1] + high[3] + high[4])/4;
+  low[2] = diff[2];
+  /*left();
   delay(300);
   stp();
   delay(100);
@@ -99,7 +144,7 @@ void bot_setup()
   delay(100);
   left();
   delay(300);
-  stp();
+  stp();*/
 }
 
 void setup() {
@@ -109,10 +154,11 @@ void setup() {
 }
 
 void loop() {
-  delay(1000);
-  delay(1000);
-  fwd();
-  delay(1000);
-  stp();
-  delay(100000);
+  find_diff();
+  if(diff[1]<(high[1]+low[2])/2)
+   while(!(diff[1]<(high[1]+low[2])/2))
+   {
+     left();
+     find_diff();
+   }
 }
